6b46d77c1fa7e24c161f6a997fada5c8e4945307
Advait Scaria <ajs244@students.calvin.edu>
2020-09-18 00:38:28 -0400

    Made a simple prototype app

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Image, StyleSheet, Text, View, Button } from 'react-native';
+import splash from './assets/splash.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Predestination:</Text>
+      <Text>Your journey begins here.</Text>
+      <View style={styles.button}>
+        <Button title="Admin" />
+      </View>
+      <View style={styles.button}>
+        <Button title="Seeker" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    padding: 10
+  }
+
+});
