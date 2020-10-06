b11cf118e1648a45665deeffda329925f494471c
haywortha <hayworth99@yahoo.com>
2020-10-06 17:10:31 -0400

    Added Testsscreen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 // import seeker screens
+
 import StartScreen from './screens/StartScreen';
 import SeekerWaitingScreen from './screens/seeker/SeekerWaitingScreen';
 import SeekerGameScreen from './screens/seeker/SeekerGameScreen';
@@ -17,25 +18,27 @@ import KeeperListScreen from './screens/keeper/KeeperListScreen';
 import KeeperGameScreen from './screens/keeper/KeeperGameScreen';
 import KeeperEditorScreen from './screens/keeper/KeeperEditorScreen';
 import KeeperWaitingScreen from './screens/keeper/KeeperWaitingScreen';
+import testScreen from './screens/TestScreen';
 
 const Stack = createStackNavigator();
 
 // for now, we are going to title each navigation screen with the function name for easier debugging
 // simple navigation setup
 export default function App() {
-    return (
-	<NavigationContainer>
-          <Stack.Navigator>            
-            <Stack.Screen name="StartScreen" component={StartScreen} options={{ title: 'StartScreen' }}/>
-            
-	    <Stack.Screen name="SeekerWaitingScreen" component={SeekerWaitingScreen} options={{ title: 'SeekerWaitingScreen' }}/>
-	    <Stack.Screen name="SeekerGameScreen" component={SeekerGameScreen} options={{ title: 'SeekerGameScreen' }}/>
-	    
-            <Stack.Screen name="KeeperListScreen" component={KeeperListScreen} options={{ title: 'KeeperListScreen' }}/>
-            <Stack.Screen name="KeeperEditorScreen" component={KeeperEditorScreen} options={{ title: 'KeeperEditorScreen' }}/>
-            <Stack.Screen name="KeeperWaitingScreen" component={KeeperWaitingScreen} options={{ title: 'KeeperWaitingScreen' }}/>
-            <Stack.Screen name="KeeperGameScreen" component={KeeperGameScreen} options={{ title: 'KeeperGameScreen' }}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="StartScreen" component={StartScreen} options={{ title: 'StartScreen' }} />
+        <Stack.Screen name="TestScreen" component={testScreen} options={{ title: 'TestScreen ' }} />
+
+        <Stack.Screen name="SeekerWaitingScreen" component={SeekerWaitingScreen} options={{ title: 'SeekerWaitingScreen' }} />
+        <Stack.Screen name="SeekerGameScreen" component={SeekerGameScreen} options={{ title: 'SeekerGameScreen' }} />
+
+        <Stack.Screen name="KeeperListScreen" component={KeeperListScreen} options={{ title: 'KeeperListScreen' }} />
+        <Stack.Screen name="KeeperEditorScreen" component={KeeperEditorScreen} options={{ title: 'KeeperEditorScreen' }} />
+        <Stack.Screen name="KeeperWaitingScreen" component={KeeperWaitingScreen} options={{ title: 'KeeperWaitingScreen' }} />
+        <Stack.Screen name="KeeperGameScreen" component={KeeperGameScreen} options={{ title: 'KeeperGameScreen' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -2,56 +2,60 @@ import React, { useState } from 'react';
 import { StyleSheet, Alert, TextInput, View, Text, Button, StatusBar, } from 'react-native';
 
 export default function StartScreen({ navigation }) {
-    
+
     const [code, setCode] = useState('');
 
     // inform user via alert an invalid code was entered
     const handleError = () => {
-	Alert.alert('Oops!', 'Code must be 6 digits', [ { text: 'Understood' }]);
+        Alert.alert('Oops!', 'Code must be 6 digits', [{ text: 'Understood' }]);
     };
 
     // ensure given code is valid
     const handleJoinPress = () => {
-	(code.length === 6) ? navigation.navigate('SeekerWaitingScreen', { code }) : handleError();
+        (code.length === 6) ? navigation.navigate('SeekerWaitingScreen', { code }) : handleError();
     };
-    
+
     return (
-	<View style={styles.container}>
-	  <View style={styles.titleSection}>
-	    <Text>PreDestination</Text>
-	    <Text>Insert Cool Logo</Text> 
-          </View>
-
-          <View style={styles.entrySection}>
-
-            <View style={styles.inputContainer}>
-              <TextInput
-	        style={styles.input}
-	        placeholder='Enter Code'
-	        keyboardType='numeric'
-	        value={code}
-	        onChangeText={val => setCode(val)} 
-              />
+        <View style={styles.container}>
+            <View style={styles.titleSection}>
+                <Text>PreDestination</Text>
+                <Text>Insert Cool Logo</Text>
             </View>
 
-              <View style={styles.joinButton}>
-	        <Button title='Join' onPress={handleJoinPress}/>
-	      </View>
-              
-              <View style={styles.createButton}>
-                <Button title="Create" onPress={() => navigation.navigate('KeeperListScreen')}/> 
-              </View>
-	                
-          </View>
-          
-	</View>
+            <View style={styles.entrySection}>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Enter Code'
+                        keyboardType='numeric'
+                        value={code}
+                        onChangeText={val => setCode(val)}
+                    />
+                </View>
+
+                <View style={styles.joinButton}>
+                    <Button title='Join' onPress={handleJoinPress} />
+                </View>
+
+                <View style={styles.createButton}>
+                    <Button title="Create" onPress={() => navigation.navigate('KeeperListScreen')} />
+                </View>
+
+                <View>
+                    <Button title="Test GPS" onPress={() => navigation.navigate('TestScreen')} />
+                </View>
+
+            </View>
+
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-	flex: 1,
-	flexDirection: 'column',
+        flex: 1,
+        flexDirection: 'column',
         alignItems: 'center',
     },
     titleSection: {
@@ -65,7 +69,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     inputContainer: {
-       
+
     },
     input: {
         fontSize: 70,
@@ -80,6 +84,6 @@ const styles = StyleSheet.create({
         width: '30%',
         textAlign: 'center',
     },
-    
+
 });
 
