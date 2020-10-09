797fe4e3464b41297bde7b27d146c3e4b2a5c4b6
Advait Scaria <ajs244@students.calvin.edu>
2020-10-09 06:34:48 -0400

    Made basic clue adding functionality to KeeperEditorScreen.js

--- screens/keeper/KeeperEditorScreen.js
+++ screens/keeper/KeeperEditorScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, Alert, KeyboardAvoidingView, TextInput, View, Text, Button, StatusBar, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, TouchableWithoutFeedback, Keyboard, Alert, KeyboardAvoidingView, TextInput, View, Text, Button, StatusBar, TouchableOpacity, FlatList } from 'react-native';
+import { globalStyles } from '../../styles/global';
 
 /**
  * KeeperEditorScreen will show running list of clues, give the ability to set down a new clue,
@@ -7,9 +8,74 @@ import { StyleSheet, Alert, KeyboardAvoidingView, TextInput, View, Text, Button,
  */
 
 export default function KeeperEditorScreen({ navigation }) {
+    [text, setText] = useState('');
+
+    const changeHandler = (val) => {
+        setText(val);
+    }
+
+    const [clues, setClues] = useState([]);
+
+    const pressHandler = (key) => {
+        setClues((prevClues) => {
+            return prevClues.filter(todo => todo.key != key)
+        })
+    }
+
+    const submitHandler = (text) => {
+        if (text) {
+            setClues((prevClues) => {
+                return [
+                    { text: text, key: Math.random().toString() },
+                    ...prevClues
+                ];
+            })
+        } else {
+            Alert.alert('OOPS', 'Please enter a clue', [
+                {
+                    text: 'Ok', onPress: () => console.log('alert closed')
+                }
+            ])
+        }
+    }
 
     return (
-	<View>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+            console.log('dismissed keyboard');
+        }}>
+            <View style={globalStyles.container}>
+                <TextInput
+                    style={globalStyles.clueInput}
+                    placeholder='new clue...'
+                    onChangeText={changeHandler}
+                    value={text}
+                />
+                <Button onPress={() => submitHandler(text)} title='add clue' />
+                <View style={globalStyles.content}>
+                    {/* <AddTodo submitHandler={submitHandler} /> */}
+                    <View style={globalStyles.list}>
+                        <FlatList
+                            data={clues}
+                            renderItem={({ item }) => (
+                                <View style={globalStyles.item}><Text>{item.text}</Text></View>
+                            )}
+                        />
+                    </View>
+                </View>
+            </View>
+        </TouchableWithoutFeedback >
+    );
+
+    return (
+        <View>
+            <TextInput
+                style={styles.input}
+                placeholder='new todo...'
+                onChangeText={changeHandler}
+                value={text}
+            />
+            <Button onPress={() => submitHandler(text)} title='add todo' color='coral' />
         </View>
     );
 }
