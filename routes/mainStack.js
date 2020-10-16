ff1f3d5f2b270a1043cc683242988a53933f6875
Jacob-Brink <jacobpeterbrink@gmail.com>
2020-10-15 21:14:15 -0400

    Added authentication context back.

--- routes/mainStack.js
+++ routes/mainStack.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Header from '../shared/header';
@@ -7,28 +7,37 @@ import { globalStyles } from '../styles/global';
 import LoginStack from '../routes/loginStack';
 import Drawer from '../routes/drawer';
 
+import { LOGIN_STATUS, AuthenticationContext } from '../src/GoogleAuthentication';
+
 const Stack = createStackNavigator();
 
 /**
  * MainStack is the entire application, including both screens before and after a user logs in
  */
 export default function MainStack({ navigation }) {
+    
+    const [loginStatus, setStatus] = useState(LOGIN_STATUS.LOADING);
+
     return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <Stack.Screen 
-                name="LoginStack"
-                component={LoginStack}
-                options={{ title: "LoginStack" }}
-                />
-            <Stack.Screen
-                name="Drawer"
-                component={Drawer}
-                options={{ title: "Drawer"}}
-            />
-        </Stack.Navigator>
+
+        <AuthenticationContext.Provider
+            value={{
+                loginStatus: LOGIN_STATUS.LOADING,
+                setLoginStatus: (newStatus) => { setStatus(newStatus); }
+            }}>
+
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}>
+
+                {
+                    ((loginStatus === LOGIN_STATUS.NEW_USER || loginStatus === LOGIN_STATUS.LOADING) && <Stack.Screen name="Login" component={LoginStack} options={{ title: 'Login' }} />) ||
+                    (loginStatus === LOGIN_STATUS.GOOGLE_USER && <Stack.Screen name="Drawer" component={Drawer} options={{ title: 'Drawer' }} />)
+                }
+
+            </Stack.Navigator>
+
+        </AuthenticationContext.Provider>
     );
 };
