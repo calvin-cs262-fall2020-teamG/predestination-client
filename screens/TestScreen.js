cb0ca64e5636c2a5be96bc4247ab25f5ae179525
ewalters47 <emw45@students.calvin.edu>
2020-11-04 09:19:33 -0500

    start class to function conversion

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -2,75 +2,77 @@ import React, { Component } from 'react';
 import { Alert, StyleSheet, Text, View, TouchableOpacity, Button, geoLocation } from 'react-native';
 import { globalStyles } from "../styles/global";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      latitude: 0,
-      longitude: 0
-    }
-  }
 
-  componentDidMount() {
-    this.watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      },
-      (error) => {
-        this.setState({ error: error.message })
-      },
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    )
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
 
-  };
+//     this.state = {
+//       latitude: 0,
+//       longitude: 0
+//     }
+//   }
 
-  findCurrentLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+//   componentDidMount() {
+//     this.watchId = navigator.geolocation.watchPosition(
+//       (position) => {
+//         this.setState({
+//           latitude: position.coords.latitude,
+//           longitude: position.coords.longitude
+//         });
+//       },
+//       (error) => {
+//         this.setState({ error: error.message })
+//       },
+//       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+//     )
 
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'permission denied'
-      });
-    }
+//   };
 
-    let location = await Location.watchPositionAsync({});
-    this.setState({ location });
-  };
+//   findCurrentLocationAsync = async () => {
+//     let { status } = await Permissions.askAsync(Permissions.LOCATION);
 
-  render() {
-    return (
-      //=====Old code from before I pulled GPS code on Oct 22=====
-      // <View style={globalStyles.container}>
-      //   {/* <Button title="location" onPress={this.getLocation}></Button> */}
-      //   <TouchableOpacity onPress={this.findCurrentLocationAsync}>
-      //     <View style={globalStyles.createButton}>
-      //       <Text style={globalStyles.buttonText}>FindMeNow!</Text>
-      //     </View>
-      //   </TouchableOpacity>
+//     if (status !== 'granted') {
+//       this.setState({
+//         errorMessage: 'permission denied'
+//       });
+//     }
 
-      //</View>
-      //Code below added after Oct 22 Meeting
-      < View style={styles.container} >
-        <Text>Location: {this.state.latitude}, {this.state.longitude}</Text>
-      </View >
-    );
-  };
-};
+//     let location = await Location.watchPositionAsync({});
+//     this.setState({ location });
+//   };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
+//   render() {
+//     return (
+//       //=====Old code from before I pulled GPS code on Oct 22=====
+//       // <View style={globalStyles.container}>
+//       //   {/* <Button title="location" onPress={this.getLocation}></Button> */}
+//       //   <TouchableOpacity onPress={this.findCurrentLocationAsync}>
+//       //     <View style={globalStyles.createButton}>
+//       //       <Text style={globalStyles.buttonText}>FindMeNow!</Text>
+//       //     </View>
+//       //   </TouchableOpacity>
+
+//       //</View>
+//       //Code below added after Oct 22 Meeting
+//       < View style={styles.container} >
+//         <Text>Location: {this.state.latitude}, {this.state.longitude}</Text>
+//       </View >
+//     );
+//   };
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   }
+// });
