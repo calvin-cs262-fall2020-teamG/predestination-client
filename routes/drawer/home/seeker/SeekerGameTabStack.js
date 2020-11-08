bb81983b7dd448b6be89e631fb3d1bbf48f1cee3
Jacob Brink <jacobpeterbrink@gmail.com>
2020-11-07 23:48:22 -0500

    Added icons to tab navigator

--- routes/drawer/home/seeker/SeekerGameTabStack.js
+++ routes/drawer/home/seeker/SeekerGameTabStack.js
@@ -2,14 +2,33 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { Text } from 'react-native';
+
 import SeekerGameScreen from '../../../../screens/seeker/SeekerGameScreen';
 import SeekerTrackerStack from './SeekerTrackerStack';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function SeekerGameTabStack() {
   return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        tabBarOptions={{
+          showLabel: false,
+        }}
+        screenOptions={({ route }) => ({
+          
+          tabBarIcon: ({ focused }) => {
+            let icon = route.name === 'Tracker' ? 'target' : 'format-list-numbered';
+            let fontColor = focused ? '#5CDB95' : 'gray';
+            
+            return (
+              <Icon name={icon} size={30} color={fontColor}/>
+            )
+          }
+        })}
+      >
         <Tab.Screen name="Stats" component={SeekerGameScreen} />
         <Tab.Screen name="Tracker" component={SeekerTrackerStack} />
       </Tab.Navigator>
