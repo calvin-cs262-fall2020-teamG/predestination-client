b626eae530ccc1304830ee26ffdaaa335785c653
Jacob Brink <jacobpeterbrink@gmail.com>
2020-11-14 23:52:54 -0500

    ESlint for Lab11

--- components/FloatingView.js
+++ components/FloatingView.js
@@ -4,56 +4,50 @@ import { StyleSheet, View, Text } from 'react-native';
  * FloatingView is a component that encourages uniform styling with some pizazz
  */
 export default function FloatingView(props) {
+  return (
 
-    return (
-
-        <View style={{
+    <View style={{
             ...styles.mainView,
-            ...props.style,
-        }}>
-            <View style={styles.title}>
-                <Text style={styles.titleText}>{props.title}</Text>
-            </View>
-            <View style={styles.content}>
-                { props.children }
-            </View>
-        </View>
+      ...props.style,
+    }}>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>{props.title}</Text>
+      </View>
+      <View style={styles.content}>
+        { props.children }
+      </View>
+    </View>
 
-    );
-
+  );
 }
 
 const styles = StyleSheet.create({
-    content: {
+  content: {
+  },
+  title: {
+    paddingBottom: 20,
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  mainView: {
+    justifyContent: 'space-around',
+    padding: 20,
+    paddingBottom: 25,
+    paddingTop: 15,
+
+    borderRadius: 15,
+    backgroundColor: 'white',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    title: {
-        paddingBottom: 20
-    },
-    titleText: {
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-    mainView: {
-        
-        justifyContent: 'space-around',
-        padding: 20,
-        paddingBottom: 25,
-        paddingTop: 15,
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
 
-        borderRadius: 15,
-        // borderBottomLeftRadius: 5,
-        // borderTopRightRadius: 5,
-
-        backgroundColor: 'white',
-
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.36,
-        shadowRadius: 6.68,
-
-        elevation: 11,
-    }
+    elevation: 11,
+  },
 });
